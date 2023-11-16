71c695776aec321d150bde20ce24c05fadac9d3c
SamuelTechHochschule <s210939@th-ab.de>
2023-11-16 11:51:58 +0100

    include Authentication in App

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import java.io.IOException;
+
+import com.example.api.AuthController;
+import com.example.api.ChecklistController;
 import com.example.auth.Authenticator;
-import com.example.auth.User;
+import com.example.auth.LDAPAuthenticator;
+
 
 import io.javalin.Javalin;
 
@@ -9,6 +14,21 @@ import io.javalin.Javalin;
 public class App 
 {
     public static void main( String[] args ) {
+
+        try {
+
+            Javalin app = Javalin.create().start(7000);
+            
+            Authenticator authenticator = new LDAPAuthenticator();
+            
+            AuthController authController = new AuthController(authenticator);
+            
+            app.post("/login", authController::login);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
         
 
     }
