61a551ac82e4ef38c71cd146c3f6a9e6ca5ff7b9
SamuelTechHochschule <s210939@th-ab.de>
2023-11-16 11:35:30 +0100

    Add AuthController

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,15 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
+import com.example.auth.Authenticator;
+import com.example.auth.User;
+
+import io.javalin.Javalin;
+
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        
+
     }
 }
---
+++ src/main/java/com/example/api/AuthController.java
@@ -0,0 +1,31 @@
+package com.example.api;
+
+import com.example.auth.Authenticator;
+import com.example.auth.User;
+import io.javalin.http.Context;
+
+public class AuthController {
+    
+    private final Authenticator authenticator;
+
+    public AuthController(Authenticator authenticator) {
+
+        this.authenticator = authenticator;
+    }
+
+    public void login(Context ctx) {
+        
+        String username = ctx.formParam("username");
+        String password = ctx.formParam("password");
+
+        User user = authenticator.login(username, password);
+
+        if(user != null) {
+
+            ctx.sessionAttribute("currentUser", user);
+            ctx.status(200).json("Login erfolgreich");
+        } else {
+            ctx.status(401).json("Login fehlgeschlagen");
+        }
+    }
+}
