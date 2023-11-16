343cf92def2d34387b4480c42fddc77849e70ded
SamuelTechHochschule <s210939@th-ab.de>
2023-11-16 10:40:10 +0100

    Added DatabaseConnector

---
+++ src/main/java/com/example/db/DatabaseConnector.java
@@ -0,0 +1,31 @@
+package com.example.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+    
+    private static final String JDBC_URL = "jdbc:postgresql://192.168.5.78:5432/Checklist_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new SQLException("Error connecting to the database", e);
+        }
+    }
+
+    public static void closeConnection(Connection connection) {
+        if(connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
