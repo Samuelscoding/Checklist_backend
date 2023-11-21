4415000041224f040d882869d536616a2f8510a6
SamuelTechHochschule <s210939@th-ab.de>
2023-11-21 12:47:09 +0100

    updated ChecklistDAO

--- src/main/java/com/example/db/ChecklistDAO.java
+++ src/main/java/com/example/db/ChecklistDAO.java
@@ -9,16 +9,26 @@ import java.util.List;
 
 public class ChecklistDAO {
 
-    public List<String> getChecklistItems() {
-        List<String> checklistItems = new ArrayList<>();
+    public List<ChecklistItem> getChecklistItems() {
+        List<ChecklistItem> checklistItems = new ArrayList<>();
 
         try(Connection connection = DatabaseConnector.getConnection();
         
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT item FROM checklist")) {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM checklist")) {
 
                 ResultSet resultSet = preparedStatement.executeQuery();
                 while (resultSet.next()) {
-                    checklistItems.add(resultSet.getString("item"));
+                    ChecklistItem item = new ChecklistItem(
+                        resultSet.getInt("id"),
+                        resultSet.getInt("number"),
+                        resultSet.getString("task"),
+                        resultSet.getString("department"),
+                        resultSet.getString("person"),
+                        resultSet.getDate("planned_date").toLocalDate(),
+                        resultSet.getDate("completed_date").toLocalDate(),
+                        resultSet.getString("signature")
+                    );
+                    checklistItems.add(item);
                 }
 
             } catch (SQLException e) {
@@ -30,14 +40,21 @@ public class ChecklistDAO {
 
     }
     
-    public void addItemToChecklist(String item) {
+    public void addItemToChecklist(ChecklistItem item) {
 
         try (Connection connection = DatabaseConnector.getConnection(); 
         
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO checklist (item) VALUES (?)")) {
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO checklist (number, task, department, person, planned_date, completed_date, signature) VALUES (?,?,?,?,?,?,?,?)")) {
 
-                preparedStatement.setString(1, item);
-                preparedStatement.executeQuery();
+                preparedStatement.setInt(1, item.getNumber());
+                preparedStatement.setString(2, item.getTask());
+                preparedStatement.setString(3, item.getDepartment());
+                preparedStatement.setString(4, item.getPerson);
+                preparedStatement.setDate(5, java.sql.Date.valueOf(item.getPlannedDate()));
+                preparedStatement.setDate(6, java.sql.Date.valueOf(item.getCompletedDate()));
+                preparedStatement.setString(7, item.getSignature());
+
+                preparedStatement.executeUpdate();
             } catch (SQLException e) {
                 e.printStackTrace();
             }
