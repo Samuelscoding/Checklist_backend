d132edc1675525a8d87bd59a173b401f4bb5004c
SamuelTechHochschule <s210939@th-ab.de>
2024-02-06 14:40:39 +0100

    delete ldap-java-configuration

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,11 +1,6 @@
 package com.example;
 
-import java.io.IOException;
-
-import com.example.api.AuthController;
 import com.example.api.ChecklistController;
-import com.example.auth.Authenticator;
-import com.example.auth.LDAPAuthenticator;
 
 
 import io.javalin.Javalin;
@@ -13,52 +8,34 @@ import io.javalin.Javalin;
 public class App 
 {
     public static void main( String[] args ) {
-        try {
-            Javalin app = Javalin.create(config -> {
-                config.plugins.enableCors(cors -> {
-                    cors.add(it -> {
-                        it.anyHost();
-                        it.allowCredentials = true;
-                        it.allowedOrigins();
-                    });
+        Javalin app = Javalin.create(config -> {
+            config.plugins.enableCors(cors -> {
+                cors.add(it -> {
+                    it.anyHost();
+                    it.allowCredentials = true;
+                    it.allowedOrigins();
                 });
-            }).start(5500);
-            
-            Authenticator authenticator = new LDAPAuthenticator();
-            
-            AuthController authController = new AuthController(authenticator);
-
-            app.post("/login", ctx -> {
-                System.out.println("Received login request");
-                authController.login(ctx);
             });
+        }).start(5500);
+        
+        ChecklistController checklistController = new ChecklistController();
+        app.get("/api/checklist", checklistController.getChecklistItems);
+        app.post("/api/checklist/addTask", checklistController.addItemToChecklist);
+        app.delete("/api/checklist/delete/{taskId}", checklistController.deleteItemFromChecklist);
+        app.put("/api/checklist/edit/{taskId}", checklistController.updateItemInChecklist);
+        app.post("/api/checklist/sendReminderEmail", checklistController.sendReminderEmail);
 
-            app.post("/logout", ctx -> {
-                System.out.println("Received logout request");
-                authController.logout(ctx);
-            });
-            
-            ChecklistController checklistController = new ChecklistController();
-            app.get("/api/checklist", checklistController.getChecklistItems);
-            app.post("/api/checklist/addTask", checklistController.addItemToChecklist);
-            app.delete("/api/checklist/delete/{taskId}", checklistController.deleteItemFromChecklist);
-            app.put("/api/checklist/edit/{taskId}", checklistController.updateItemInChecklist);
-            app.post("/api/checklist/sendReminderEmail", checklistController.sendReminderEmail);
+        app.post("/api/checklist/import", checklistController.importChecklistItems);
 
-            app.post("/api/checklist/import", checklistController.importChecklistItems);
+        app.get("/api/versions", checklistController.getVersions);
+        app.post("api/version/addVersion", checklistController.addVersion);
+        app.put("/api/version/editVersion/{id}", checklistController.editVersion);
+        app.delete("/api/version/deleteVersion/{versionName}", checklistController.deleteVersion);
 
-            app.get("/api/versions", checklistController.getVersions);
-            app.post("api/version/addVersion", checklistController.addVersion);
-            app.put("/api/version/editVersion/{id}", checklistController.editVersion);
-            app.delete("/api/version/deleteVersion/{versionName}", checklistController.deleteVersion);
-
-            app.exception(Exception.class, (e, ctx) -> {
-                e.printStackTrace();
-                ctx.status(500).json("Internal Server Error");
-            });
-        } catch (IOException e) {
+        app.exception(Exception.class, (e, ctx) -> {
             e.printStackTrace();
-        }
+            ctx.status(500).json("Internal Server Error");
+        });
 
     }
 }
